3c626e03a015db725a1d89cef84e2d13800ccd23
Abramov Vadim <vadim@gmail.com>
2025-10-27 17:35:48 +0300

    Готовый дизайн Формы 2

--- Form2.cs
+++ Form2.cs
@@ -16,5 +16,10 @@ namespace PR08_Maletin_AbramovVadim
         {
             InitializeComponent();
         }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
